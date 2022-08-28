ae4f4a3eb70d5f97dde0f5e18311803d82dd4f5c
Arjun Acharya <legendarjun333@gmail.com>
2022-08-28 17:03:28 +0545

    Inital commit with simple migration of categories table and model setup and Base Controller setup

---
+++ app/Http/Controllers/Api/BaseController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+     /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendResponse($result, $message){
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message
+        ];
+        return response()->json($response, 200);
+    }
+    /**
+     * return error response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendError($error, $errorMessages = [])
+    {
+    	$response = [
+            'success' => false,
+            'message' => $error,
+        ];
+        if(!empty($errorMessages)){
+            $response['data'] = $errorMessages;
+        }
+        return response()->json($response, 404);
+    }
+}
---
+++ app/Http/Resources/Category.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Category extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'category_name' => $this->category_name,
+            'slug' => $this->slug,
+            'category_image' => $this->category_image,
+            'category_description' => $this->category_description,
+            'created_at' => $this->created_at->format('m/d/Y'),
+            'updated_at' => $this->updated_at->format('m/d/Y'),
+        ];
+    }
+}
